8f62fb6db21ab43b0feb8bf8ef70fe67f6707854
Senthuran Elangovan <senthuran@pikanite.com>
2019-10-24 00:22:21 +0530

    Axios Added

--- pages/support.jsx
+++ pages/support.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import AuthLaylout from '../Hoc/AuthLayout'
+import axios from 'axios'
 import { getSingleNav } from "../Services/fakeUserService";
 import Content from '../components/Common/Content'
 
@@ -9,21 +10,31 @@ class Support extends Component {
         metaTag: ""
     }
 
-    // componentDidMount() {
-    //     const singleNav = getSingleNav("support")
-    //     this.setState({
-    //         name: singleNav.name,
-    //         metaTag: singleNav.metaTag
-    //     })
-    // }
+    componentDidMount() {
+        // const singleNav = getSingleNav("support")
+        // this.setState({
+        //     name: singleNav.name,
+        //     metaTag: singleNav.metaTag
+        // })
 
-    static async getInitialProps({ req }) {
-        const res = getSingleNav("support")
-        return {
-            name: res.name,
-            metaTag: res.metaTag
-        }
+        // const res =  axios.get('https://m-city-7c464.firebaseio.com/metatest.json')
+        //     .then((response) => {
+        //         console.log(response.data);
+        //     });
     }
+
+    // static async getInitialProps({ req }) {
+    //     // const res = await axios.get('https://m-city-7c464.firebaseio.com/metatest.json')
+    //     //     .then((response) => {
+    //     //         console.log(response.data);
+    //     //     });
+
+    //     const res = await getSingleNav("support")
+    //     return {
+    //         name: res.name,
+    //         metaTag: res.metaTag
+    //     }
+    // }
 
     render() {
         const {
@@ -47,6 +58,26 @@ class Support extends Component {
                 />
             </AuthLaylout>
         )
+    }
+}
+
+Support.getInitialProps = async ({ req }) => {
+    const res1 = await axios.get('https://m-city-7c464.firebaseio.com/metatest.json')
+    // .then((response) => {
+    //     return {
+    //         name: response.data.name,
+    //         metaTag: response.data.metaTag,
+    //     }
+    // });
+
+    // console.log(res1.data.name);
+
+
+    // const res = await getSingleNav("support")
+
+    return {
+        name: res1.data.name,
+        metaTag: res1.data.metaTag
     }
 }
 
