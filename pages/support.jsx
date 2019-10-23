a3b5f58e833c8c37a86f5297c312dd8257a35eaf
Senthuran Elangovan <senthuran@pikanite.com>
2019-10-23 23:07:29 +0530

    Support Page modifid

--- pages/support.jsx
+++ pages/support.jsx
@@ -4,42 +4,51 @@ import { getSingleNav } from "../Services/fakeUserService";
 import Content from '../components/Common/Content'
 
 class Support extends Component {
-  state = {
-    name: "",
-    metaTag: ""
-  }
+    state = {
+        name: "",
+        metaTag: ""
+    }
 
-  componentDidMount() {
-    const singleNav = getSingleNav("support")
-    this.setState({
-      name: singleNav.name,
-      metaTag: singleNav.metaTag
-    })
-  }
+    componentDidMount() {
+        const singleNav = getSingleNav("support")
+        this.setState({
+            name: singleNav.name,
+            metaTag: singleNav.metaTag
+        })
+    }
 
-  render() {
-    const {
-      title,
-      image,
-      shortDescription,
-      description
-    } = this.state.metaTag
+    render() {
+        const {
+            title,
+            image,
+            shortDescription,
+            description
+        } = this.props.metaTag
 
-    const { name } = this.state
-    return (
-      <AuthLaylout
-        name={name}
-        title={title}
-        shortDescription={shortDescription}
-        image={image}>
-        <Content
-          name={title}
-          imageUrl={image}
-          description={description}
-        />
-      </AuthLaylout>
-    )
-  }
+        const { name } = this.props
+        return (
+            <AuthLaylout
+                name={name}
+                title={title}
+                shortDescription={shortDescription}
+                image={image}>
+                <Content
+                    name={title}
+                    imageUrl={image}
+                    description={description}
+                />
+            </AuthLaylout>
+        )
+    }
+}
+
+
+Page.getInitialProps = async ({ req }) => {
+    const res = getSingleNav("support")
+    return {
+        name: res.name,
+        metaTag: res.metaTag
+    }
 }
 
 export default Support
